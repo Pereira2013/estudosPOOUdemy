31478075fb03496acf16605f2f47e855b4a5dd7a
robsonpereira <robson.pereira@sensedia.com>
2023-01-15 11:27:50 -0300

    fix:  updated run with lambda.

--- src/exerciciosOO/application/ProgramEmployeeList.java
+++ src/exerciciosOO/application/ProgramEmployeeList.java
@@ -21,6 +21,10 @@ public class ProgramEmployeeList {
             System.out.println("Employee #" + (i+1)+ ":");
             System.out.print("Id: ");
             Integer id = sc.nextInt();
+            while (hasId(list, id)) {
+                System.out.println("Id already taken! Try again: ");
+                 id = sc.nextInt();
+            }
             System.out.print("Name: ");
             sc.nextLine();// usado para consumir o buffer de quebra de linha que o nextInt deixa em aberto
             String name = sc.nextLine();
@@ -33,32 +37,30 @@ public class ProgramEmployeeList {
         System.out.println();
         System.out.print("Enter the employee id the will have salary increase: ");
         int idSalary = sc.nextInt();
-        Integer pos = position(list, idSalary);
+        EmployeeList emp = list.stream().filter(x -> x.getId() == idSalary).findFirst().orElse(null);
 
-        if (pos == null) {
+        if (emp == null) {
             System.out.println("This id does not exist!");
         }
         else {
             System.out.print("Enter the porcent: ");
             double porcent = sc.nextDouble();
-            list.get(pos).increaseSalary(porcent);
+            emp.increaseSalary(porcent);
         }
         System.out.println();
         System.out.println("List of employee:");
 
-        for(EmployeeList emp : list) {
-            System.out.println(emp);
+        for(EmployeeList e : list) {
+            System.out.println(e);
         }
 
         sc.close();
     }
 
-    public static Integer position (List<EmployeeList> list, int id) {
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).getId() == id) {
-                return 1;
-            }
-        }
-        return null;
+    public static boolean hasId(List<EmployeeList> list, int id) {
+        EmployeeList emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+        return emp != null;
     }
+
+
 }
