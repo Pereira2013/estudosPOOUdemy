bad694f946565537f1f24a72a7b639417ae56e03
robsonpereira <robson.pereira@sensedia.com>
2023-02-12 20:06:04 -0300

    feat: added Upcasting and Downcasting fixation exercise.

---
+++ src/exerciciosOO/application/ProgramAccount.java
@@ -0,0 +1,37 @@
+package exerciciosOO.application;
+
+import exerciciosOO.entities.SaveAccount;
+
+public class ProgramAccount {
+    public static void main(String[] args) {
+        Account acc = new Account(1001, "Alex", 0.0);
+        BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+
+        // UPCASTING
+
+        Account acc1 = bacc; // Account aceita atribuir BusinessAccount pois é uma conta
+        Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+        Account acc3 = new SaveAccount(1004, "Anna", 0.0, 0.01);
+
+
+        // DOWNCASTING
+        BusinessAccount acc4 = (BusinessAccount)acc2; // para isso tem que fazer o casting manual
+        acc4.loan(100.0); // para poder acessar a operacão loan
+
+        //BusinessAccount acc5 = (BusinessAccount)acc3; // essa operacão não é permitida, mas o compilador não sabe definir
+
+        if (acc3 instanceof BusinessAccount) {
+            BusinessAccount acc5 = (BusinessAccount)acc3;
+            acc5.loan(200.0);
+            System.out.println("loan!");
+        }
+        if (acc3 instanceof SaveAccount) {
+            SaveAccount acc5 = (SaveAccount)acc3;
+            acc5.updateBalance();
+            System.out.println("Updated!");
+        }
+
+
+
+    }
+}
